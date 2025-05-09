5014b57eb004fe09b6ed66a0d2038b7e28bc0561
Mena Maged <menaxmaged@gmail.com>
2025-05-09 05:26:09 +0300

    Base With Widget Workssss

---
+++ ios/StreakWidget/StreakWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StreakWidgetBundle.swift
+//  StreakWidget
+//
+//  Created by Mena Maged on 09/05/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StreakWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        StreakWidget()
+    }
+}
